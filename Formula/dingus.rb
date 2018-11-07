c77c1af6474d0c25a2edc3f8fbf5d8d18a342c87
David Lewis <david@weassemble.com>
2018-11-06 18:34:10 -0800

    Update Dingus to 0.5.6

--- Formula/dingus.rb
+++ Formula/dingus.rb
@@ -2,8 +2,8 @@ class Dingus < Formula
   desc "Dingus - the friendly environment variable manager"
   homepage "https://github.com/davidarmstronglewis/dingus"
 
-  url "https://github.com/davidarmstronglewis/homebrew-dingus/raw/master/releases/dingus-0.5.5.zip"
-  sha256 "86ec78806d85b5c4a00cdf79bf1cf11a779667dc6432e5b593bcda296ecb8b8e"
+  url "https://github.com/davidarmstronglewis/homebrew-dingus/raw/master/releases/dingus-0.5.6.zip"
+  sha256 "c7a0d860001799ac0d4441143e75e208c91aeb236441cf4caba55c913d8162b8"
 
   def install
     bin.install "dingus"
